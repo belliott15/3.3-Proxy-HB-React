39e9c147ea63691d9b80f5f2545ac5db089d2b3a
Beau Elliott <belliott1516@gmail.com>
2022-05-17 16:02:54 -0700

    added loading spinner

--- src/App.js
+++ src/App.js
@@ -2,39 +2,53 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import getPokemon from './services/fetch.utils';
 import PokemonList from './PokemonList';
+import Spinner from './Spinner';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
   const [query, setQuery] = useState('char');
+  const [loading, setLoading] = useState(false);
 
+  
   useEffect(() => {
+    
     async function getAllPokemon(){
+      setLoading(true);
       const response = await getPokemon(query);
       setPokemon(response.data.results);
+      setLoading(false);
     }
-    
     getAllPokemon();
-  }, [query]);
-
+    
+  }, []);
+  
   
 
   async function handleQuery(e){
     e.preventDefault();
-
-    async function getAllPokemon(){
+    
+    async function getAllPokemon(){setLoading(true);
       const { data } = await getPokemon(query);
-      setPokemon(data.results);
+      setPokemon(data.results); 
+      setLoading(false);  
     }
+    
     getAllPokemon();
+   
   }
   return (
     <div className="App">
       <header className="App-header">
-        <form onSubmit={handleQuery}>
-          <input required onChange={(e) => setQuery(e.target.value)} />
-          <button>Search</button>
-        </form>
-        <PokemonList pokemon={pokemon}/>
+        {loading === true ? <Spinner /> 
+          : <div>
+            <h1>Pokemon Search</h1>
+            <form onSubmit={handleQuery}>
+              <input required onChange={(e) => setQuery(e.target.value)} />
+              <button>Search</button>
+            </form>
+            <PokemonList pokemon={pokemon}/> 
+          </div>
+        }
       </header>
     </div>
   );
---
+++ src/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Spinner() {
+  return (
+    <div className='loading-spinner'>
+      <h1>Loading...</h1>
+    </div>
+  );
+}
